1eeec251ccf073e5c2c70755a0c170e34a08aeec
Nerevarishe <JIyHaTuK@gmail.com>
2019-07-28 13:35:44 +0300

    Models for another modules

--- app/models.py
+++ app/models.py
@@ -18,3 +18,37 @@ class User(Document):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+# class TokenBlackList(Document):
+#     jti = fl.StringField(max_length=36)
+#     token_type = fl.StringField(max_length=10)
+#     user_identity = fl.StringField(max_length=50)
+#     revoked = fl.BooleanField()
+#     expires = fl.DateTimeField()
+
+
+class NewsPost(Document):
+    post_body = fl.StringField()
+    author = fl.ReferenceField(User)
+    date_created = fl.DateTimeField(default=datetime.utcnow) # indexed
+    date_edited = fl.DateTimeField(default=datetime.utcnow)
+
+
+class DefecturaCard(Document):
+    drug_name = fl.StringField(max_length=60)
+    comment = fl.StringField(max_length=200)
+    employee_name = fl.StringField(max_length=30)
+    in_zd = fl.BooleanField(default=False) # indexed
+    date = fl.DateField() # indexed
+    user = fl.ReferenceField(User)
+    date_created = fl.DateTimeField(default=datetime.utcnow)
+    date_edited = fl.DateTimeField(default=datetime.utcnow)
+
+
+class DeferredDrug(Document):
+    drug_name = fl.StringField(max_length=60) # indexed
+    drug_amount = fl.IntField(max_value=100)
+    comment = fl.StringField(max_length=200)
+    date_created = fl.DateTimeField(default=datetime.utcnow)
+    date_edited = fl.DateTimeField(default=datetime.utcnow)
