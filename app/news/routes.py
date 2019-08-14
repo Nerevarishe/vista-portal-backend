cbefe9a60bd16c7f81c75ae8c98b0a69483615c2
Nerevarishe <JIyHaTuK@gmail.com>
2019-08-14 11:36:42 +0300

    adding update news post endpoint

--- app/news/routes.py
+++ app/news/routes.py
@@ -1,26 +1,27 @@
 from flask import jsonify, request, abort, current_app
 # from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from datetime import datetime
+
 from app.news import bp
 from app.models import NewsPost
 
 
 @bp.route('/', methods=['GET'])
 def get_news_posts():
+
     """ Return paginated news posts """
 
-    # Set page and per_page vars from request for pagination
+    # Set page and per_page vars from request for pagination or use default values
     page = 1
     per_page = current_app.config['NEWS_POST_PER_PAGE']
 
-    if 'page' in request.args:
-        if request.args['page'] != '':
-            page = int(request.args['page'])
-    if 'per_page' in request.args:
-        if request.args['per_page'] != '':
-            per_page = int(request.args['per_page'])
+    if 'page' in request.args and request.args['page'] != '':
+        page = int(request.args['page'])
+    if 'per_page' in request.args and request.args['per_page'] != '':
+        per_page = int(request.args['per_page'])
 
-    # Get paginated posts
+    # Get paginated posts page
     _posts = []
     posts = NewsPost.objects.paginate(page=page, per_page=per_page)
     for post in posts.items:
@@ -65,16 +66,30 @@ def add_news_post():
 
 @bp.route('/<news_post_id>', methods=['PUT'])
 def update_news_post(news_post_id):
-    """ Return True if post updated """
-    return jsonify({
-        "msg": "update_news_post"
-    })
+
+    """ Return OK if post updated """
+
+    if 'postBody' in request.json and request.json['postBody'] != '':
+        post = NewsPost.objects.get_or_404(id=news_post_id)
+        if post.post_body != request.json['postBody']:
+            post.post_body = request.json['postBody']
+            post.date_edited = datetime.utcnow()
+            post.save()
+
+            return jsonify({
+                "msg": "OK"
+            })
+        return jsonify({
+            "msg": "OK"
+        })
+    abort(400)
 
 
 @bp.route('/<news_post_id>', methods=['DELETE'])
 def delete_news_post(news_post_id):
 
     """ Return True if post deleted """
+
     post = NewsPost.objects.get_or_404(id=news_post_id)
     post.delete()
     return jsonify({
