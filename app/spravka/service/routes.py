31fe254f69061ff4c816891bdaf9731d87744d58
Nerevarishe <JIyHaTuK@gmail.com>
2019-08-19 22:37:26 +0300

    Service Centers module endpoints

--- app/spravka/service/routes.py
+++ app/spravka/service/routes.py
@@ -1,29 +1,78 @@
-from flask import jsonify, request, abort, current_app
+from flask import jsonify, request, abort
 
 from app.spravka import bp
-from app.models import ServiceCenterList
+from app.models import ServiceCenter
 
 
 @bp.route('/service/', methods=['GET'])
 def get_all_services():
-    pass
+
+    """ Return all records """
+
+    service_centers = ServiceCenter.objects.all()
+
+    return jsonify({
+        "service_centers": service_centers
+    })
 
 
 @bp.route('/service/<service_id>', methods=['GET'])
 def get_service(service_id):
-    pass
+
+    """ Return one record by it ID """
+
+    service_center = ServiceCenter.objects.get_or_404(id=service_id)
+
+    return jsonify({
+        "service_center": service_center
+    })
 
 
 @bp.route('/service/', methods=['POST'])
 def add_service():
-    pass
+
+    """ Return created record ID """
+
+    service_center = ServiceCenter()
+
+    for field in ServiceCenter.fields(ServiceCenter):
+        if field not in request.json:
+            abort(400)
+        service_center[field] = request.json[field]
+    service_center.save()
+
+    return jsonify({
+        "msg": "OK",
+        "id": str(service_center.id)
+    }), 201
 
 
 @bp.route('/service/<service_id>', methods=['PUT'])
 def update_service(service_id):
-    pass
+
+    """ Update record by it ID and return OK """
+
+    service_center = ServiceCenter.objects.get_or_404(id=service_id)
+
+    for field in ServiceCenter.fields(ServiceCenter):
+        if field not in request.json:
+            abort(400)
+        service_center[field] = request.json[field]
+    service_center.save()
+
+    return jsonify({
+        "msg": "OK",
+    })
 
 
-@bp.route('/services/<service_id>', methods=['DELETE'])
+@bp.route('/service/<service_id>', methods=['DELETE'])
 def delete_service(service_id):
-    pass
+
+    """ Delete record by it ID and return OK """
+
+    service_center = ServiceCenter.objects.get_or_404(id=service_id)
+    service_center.delete()
+
+    return jsonify({
+        "msg": "OK"
+    })
