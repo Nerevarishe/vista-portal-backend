0f8591e9217a14c7fb4f16e2ea1e38735bb84038
Nerevarishe <JIyHaTuK@gmail.com>
2019-12-19 12:51:33 +0300

    news posts module

--- app/news/routes.py
+++ app/news/routes.py
@@ -1,11 +1,12 @@
 from flask import jsonify, request, abort, current_app
+from flask_jwt_extended import jwt_required
 
 from datetime import datetime
 
 from app.news import bp
 from app.models import NewsPost
 
-from utils.check import is_request_json_field_exist
+from utils.check import is_request_json_field_exist, is_request_args_field_exist
 
 
 @bp.route('/', methods=['GET'])
@@ -17,13 +18,15 @@ def get_news_posts():
     page = 1
     per_page = current_app.config['NEWS_POST_PER_PAGE']
 
-    if 'page' in request.args and request.args['page'] != '':
+    if is_request_args_field_exist('page'):
         page = int(request.args['page'])
-    if 'perPage' in request.args and request.args['per_page'] != '':
+    if is_request_args_field_exist('per_page'):
         per_page = int(request.args['per_page'])
 
     # Get paginated posts page
     _posts = []
+
+    # Get all paginated posts ordered by date descending and add them to _posts list
     posts = NewsPost.objects.order_by('-date_created').paginate(page=page, per_page=per_page)
     for post in posts.items:
         _posts.append(post)
@@ -33,9 +36,7 @@ def get_news_posts():
         "postsPageHasNext": posts.has_next,
         "postsPageNextPageNumber": posts.next_num,
         "postsPageHasPrev": posts.has_prev,
-        "postsPagePrevPageNumber": posts.prev_num,
-        "dateCreated": '',
-        "dateEdited": ''
+        "postsPagePrevPageNumber": posts.prev_num
     })
 
 
@@ -46,13 +47,12 @@ def get_news_post(news_post_id):
 
     post = NewsPost.objects.get_or_404(id=news_post_id)
     return jsonify({
-        "post": post,
-        "dateCreated": '',
-        "dateEdited": ''
+        "post": post
     })
 
 
 @bp.route('/', methods=['POST'])
+@jwt_required
 def add_news_post():
 
     """ Return created record ID """
@@ -70,11 +70,12 @@ def add_news_post():
 
 
 @bp.route('/<news_post_id>', methods=['PUT'])
+@jwt_required
 def update_news_post(news_post_id):
 
     """ Update record by it ID and return OK """
 
-    if 'postBody' in request.json and request.json['postBody'] != '':
+    if is_request_json_field_exist('postBody') and request.json['postBody'] != '':
         post = NewsPost.objects.get_or_404(id=news_post_id)
         if post.post_body != request.json['postBody']:
             post.post_body = request.json['postBody']
@@ -91,6 +92,7 @@ def update_news_post(news_post_id):
 
 
 @bp.route('/<news_post_id>', methods=['DELETE'])
+@jwt_required
 def delete_news_post(news_post_id):
 
     """ Delete record by it ID and return OK """
